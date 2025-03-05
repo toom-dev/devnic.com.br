c860ad15bcc0b709a05e3442d28cffcf37a97491
Clayton <claytonrodriguesmonteagudojr@gmail.com>
2025-03-05 16:55:54 -0300

    Add Google Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-EBDZE154W3" />
     </html>
   );
 }
